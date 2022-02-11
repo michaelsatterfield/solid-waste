19b61ee26503fc64f089e58696d26e0503c45441
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-02-11 16:43:40 -0600

    removed categories icons

--- src/views/Home.js
+++ src/views/Home.js
@@ -11,49 +11,49 @@ export default function Home() {
   return (
     <div style={{ paddingTop: "34px", paddingBottom: "34px" }}>
       <SearchBar />
-      <div className="popularCategories">
-        <p className="popular_searches_text">Categories</p>
-        {context.categories.length >= 12 ? (
-          <Grid container spacing={7}>
-            {context.categories.slice(0, limiter).map((cat) => {
-              return (
-                <Grid
-                  className="category_card"
-                  key={cat.name}
-                  item
-                  md={4}
-                  lg={3}
-                  xs={6}
-                >
-                  <Link
-                    style={{ textDecoration: "none", color: "#000" }}
-                    to={`category/${cat.id}`}
-                  >
-                    <img
-                      src={test_image}
-                      className="category_image"
-                      alt="category_image"
-                    />
-                    <p className="category_name">{cat.name}</p>
-                  </Link>
-                </Grid>
-              );
-            })}
-          </Grid>
-        ) : (
-          <p className="loadingText">Loading..</p>
-        )}
+      {/*<div className="popularCategories">*/}
+      {/*  <p className="popular_searches_text">Categories</p>*/}
+      {/*  {context.categories.length >= 12 ? (*/}
+      {/*    <Grid container spacing={7}>*/}
+      {/*      {context.categories.slice(0, limiter).map((cat) => {*/}
+      {/*        return (*/}
+      {/*          <Grid*/}
+      {/*            className="category_card"*/}
+      {/*            key={cat.name}*/}
+      {/*            item*/}
+      {/*            md={4}*/}
+      {/*            lg={3}*/}
+      {/*            xs={6}*/}
+      {/*          >*/}
+      {/*            <Link*/}
+      {/*              style={{ textDecoration: "none", color: "#000" }}*/}
+      {/*              to={`category/${cat.id}`}*/}
+      {/*            >*/}
+      {/*              <img*/}
+      {/*                src={test_image}*/}
+      {/*                className="category_image"*/}
+      {/*                alt="category_image"*/}
+      {/*              />*/}
+      {/*              <p className="category_name">{cat.name}</p>*/}
+      {/*            </Link>*/}
+      {/*          </Grid>*/}
+      {/*        );*/}
+      {/*      })}*/}
+      {/*    </Grid>*/}
+      {/*  ) : (*/}
+      {/*    <p className="loadingText">Loading..</p>*/}
+      {/*  )}*/}
 
-        <p
-          onClick={() => setLimiter(context.categories.length)}
-          className="seeMoreBtn"
-          style={{
-            display: limiter === context.categories.length ? "none" : "block",
-          }}
-        >
-          {context.categories.length >= 12 ? "See All" : ""}
-        </p>
-      </div>
+      {/*  <p*/}
+      {/*    onClick={() => setLimiter(context.categories.length)}*/}
+      {/*    className="seeMoreBtn"*/}
+      {/*    style={{*/}
+      {/*      display: limiter === context.categories.length ? "none" : "block",*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    {context.categories.length >= 12 ? "See All" : ""}*/}
+      {/*  </p>*/}
+      {/*</div>*/}
     </div>
   );
 }
