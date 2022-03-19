d4785ff32288b4dff38dc480ca47aae163f6f4e3
michael_satterfield <michael.f.satterfield@gmail.com>
2022-03-18 23:19:00 -0500

    3

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Autocompleted from "./AutoCompleted";
-//import banner_img from '../assets/banner.jpg'
-//let test_banner_img = 'https://solid-waste-search.web.app/'
 
 export default function SearchBar() {
   return (
