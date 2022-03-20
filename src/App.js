ef757975c41645f8bc2f0ceeb78da84eaace5b09
michael_satterfield <michael.f.satterfield@gmail.com>
2022-03-20 13:00:13 -0500

    3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import Main from "./views/Main";
 import Header from "./components/Header";
 import MyProvider from "./state/MyProvider";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+// import NotFound from "./components/NotFound";
 import React from "react";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/material/undefined" component={NotFound} />
+        {/* <Route exact path="/material/undefined" component={NotFound} /> */}
         <MyProvider>
           <Header />
           <Main />
